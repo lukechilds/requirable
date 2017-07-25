c59e9d9184cdb889d7dbd30e055d51c9d63239e9
Luke Childs <lukechilds123@gmail.com>
2017-07-25 19:41:59 +0100

    Test requiring package name requires root package

--- test/requireable.js
+++ test/requireable.js
@@ -5,3 +5,8 @@ test.serial(`outputs success info`, t => {
 	t.true(output.success);
 	t.is(output.name, 'requireable');
 });
+
+test.serial(`requiring package name requires root package`, t => {
+	// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved
+	t.true(typeof require('requireable') === 'function');
+});
