a9f3fd2d8bd5327aa4677532b1fe0596507bc9fd
Luke Childs <lukechilds123@gmail.com>
2017-07-26 12:05:50 +0100

    Revert to using mock-require but return require instead of wrapping in fn

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 'use strict';
 
-const Module = require('module');
 const findRoot = require('find-root');
+const mock = require('mock-require');
 
 const pkg = { success: false };
 
 try {
 	pkg.path = findRoot(process.cwd());
 	pkg.name = require(pkg.path + '/package.json').name;
-	const origRequire = Module.prototype.require;
-	Module.prototype.require = pkgName => pkgName === pkg.name ? origRequire(pkg.path) : origRequire(pkgName);
+	mock(pkg.name, require(pkg.path));
 	pkg.success = true;
 } catch (err) {}
 
